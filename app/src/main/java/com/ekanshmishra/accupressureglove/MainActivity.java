9dc62b8b8eeb0bb5b2bec81ac5d00c9f55e527a8
ekanshmishra <ekanshm007@gmail.com>
2017-05-09 02:44:46 +0530

    First Commit

---
+++ app/src/main/java/com/ekanshmishra/accupressureglove/MainActivity.java
@@ -0,0 +1,79 @@
+package com.ekanshmishra.accupressureglove;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+    private String bluetoothClientAddress = "";
+
+    protected String EXTRA_ADDRESS = "address";
+
+    @ViewById(R.id.b_eye) Button button_eye;
+    @ViewById(R.id.b_heart) Button button_heart;
+    @ViewById(R.id.b_kidney) Button button_kidney;
+    @ViewById(R.id.b_sinus) Button button_sinus;
+    @ViewById(R.id.b_stomach) Button button_stomach;
+    @ViewById(R.id.b_stop) Button button_stop;
+
+    @AfterViews
+    protected void afterViews() {
+        bluetoothClientAddress = getIntent().getStringExtra(FirstPage.EXTRA_ADDRESS);
+    }
+
+    @Click(R.id.b_eye)
+    protected void buttonEyeClicked() {
+        Intent intent = new Intent(MainActivity.this, ConnectionActivity_.class);
+        intent.putExtra("what_clicked", "eye");
+        intent.putExtra(FirstPage.EXTRA_ADDRESS, bluetoothClientAddress);
+        startActivity(intent);
+    }
+
+    @Click(R.id.b_heart)
+    protected void buttonHeartClicked() {
+        Intent intent = new Intent(MainActivity.this, ConnectionActivity_.class);
+        intent.putExtra("what_clicked", "heart");
+        intent.putExtra(EXTRA_ADDRESS, bluetoothClientAddress);
+        startActivity(intent);
+    }
+
+    @Click(R.id.b_kidney)
+    protected void buttonKidneyClicked() {
+        Intent intent = new Intent(MainActivity.this, ConnectionActivity_.class);
+        intent.putExtra("what_clicked", "kidney");
+        intent.putExtra(EXTRA_ADDRESS, bluetoothClientAddress);
+        startActivity(intent);
+    }
+
+    @Click(R.id.b_sinus)
+    protected void buttonSinusClicked() {
+        Intent intent = new Intent(MainActivity.this, ConnectionActivity_.class);
+        intent.putExtra("what_clicked", "sinus");
+        intent.putExtra(EXTRA_ADDRESS, bluetoothClientAddress);
+        startActivity(intent);
+    }
+
+    @Click(R.id.b_stomach)
+    protected void buttonStomachClicked() {
+        Intent intent = new Intent(MainActivity.this, ConnectionActivity_.class);
+        intent.putExtra("what_clicked", "stomach");
+        intent.putExtra(EXTRA_ADDRESS, bluetoothClientAddress);
+        startActivity(intent);
+    }
+
+    @Click(R.id.b_stop)
+    protected void buttonStopClicked() {
+        Intent intent = new Intent(MainActivity.this, ConnectionActivity_.class);
+        intent.putExtra("what_clicked", "stop");
+        intent.putExtra(EXTRA_ADDRESS, bluetoothClientAddress);
+        startActivity(intent);
+    }
+}
